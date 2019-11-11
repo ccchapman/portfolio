2ed35a06cf3dab1c993ab3c4b7dc7b762ce05faa
Christopher Chapman <42548502+ccchapman@users.noreply.github.com>
2019-11-10 19:44:51 -0800

    Add default title with React Helmet
    Closes #11

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,4 @@
+import Helmet from "react-helmet";
 import s from "styled-components";
 import tw from "tailwind.macro";
 
@@ -12,6 +13,12 @@ const Layout = ({
   children,
 }) => (
   <LayoutWrapper>
+    <Helmet>
+      <title>
+        Chris Chapman's Portfolio
+      </title>
+    </Helmet>
+
     <style jsx global>{`
       @import url('https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css');
       @import url('https://fonts.googleapis.com/css?family=Fira+Code&display=swap');
